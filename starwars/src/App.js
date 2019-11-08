f164f939425ea620aac555c8fffbdf9c73df97ea
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2019-11-08 14:51:49 -0800

    carousel

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import StarWarsList from "./components/StarWarsList"
+import Carousel from "./components/Carousel"
 
 import './App.css';
+import PhotoCarousel from './components/Carousel';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,9 +16,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-        
+        <PhotoCarousel />
           <StarWarsList />
-        
+          
     </div>
     
   );
---
+++ starwars/src/components/Carousel.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+  {
+    src: 'https://images.unsplash.com/photo-1472457847783-3d10540b03d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    altText: 'Family fun',
+    caption: 'Weeeeeeeeeeeeeee! DADDY LOOK AT ME!'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    altText: 'Long walks',
+    caption: 'I may not look it, but I have feelings deep down inside'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1558793576-9bd76e5c4021?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    altText: 'The Force',
+    caption: 'That takes some really good abs to pull off!'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1558793582-f025c3d3a227?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    altText: 'Uh oh',
+    caption: 'Uh oh!'
+  }
+
+
+];
+
+const PhotoCarousel = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default PhotoCarousel;
